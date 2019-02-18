7d87d415e50287900fc560dada88e72ffcae2298
Varditn <38429204+TAvardit@users.noreply.github.com>
2019-02-18 13:23:53 +0100

    Running hard delete after all other tasks (#6)
    * Running hard delete after all other tasks

--- cleanup-job.sh
+++ cleanup-job.sh
@@ -53,10 +53,22 @@ function escape_url {
 
 function run_final_cleanup {
     #  Triggering  needed tasks including hard cleanup task.
-    for i in "$@" ;
+    last=${#@};
+    i=0;
+    for d in "$@" ;
     do
-        echo "Running Task with ID $i"
-        curl -i -u ${NEXUS_AUTH} -X POST "${NEXUS_URL}/service/rest/v1/tasks/$i/run" -H "accept: application/json";
+        i=$((i+1));
+        if [ $i -eq $last ];
+        then
+        # optimisation needed: check the status of the previous tasks and then run the last one
+            echo "Waiting for other tasks before compacting blob store";
+            sleep 300;
+            echo "Running Task with ID $d";
+            curl -i -u ${NEXUS_AUTH} -X POST "${NEXUS_URL}/service/rest/v1/tasks/$d/run" -H "accept: application/json";
+        else
+            echo "Running Task with ID $d";
+            curl -i -u ${NEXUS_AUTH} -X POST "${NEXUS_URL}/service/rest/v1/tasks/$d/run" -H "accept: application/json";
+        fi
     done
 }
 
